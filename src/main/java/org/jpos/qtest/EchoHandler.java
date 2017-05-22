ce64d7e7c96d57a6332348fa4082a2fbd6388237
shaman2001 <Vladimir_Kotovich@epam.com>
2017-05-22 15:23:56 +0300

    save changes

--- src/main/java/org/jpos/qtest/EchoHandler.java
+++ src/main/java/org/jpos/qtest/EchoHandler.java
@@ -1,12 +1,10 @@
 package org.jpos.qtest;
 
-import org.jpos.iso.ISOException;
-import org.jpos.iso.ISOMsg;
-import org.jpos.iso.ISORequestListener;
-import org.jpos.iso.ISOSource;
+import org.jpos.iso.*;
 import org.jpos.util.Log;
 
 import java.io.IOException;
+import java.util.Date;
 
 
 public class EchoHandler extends Log implements ISORequestListener {
@@ -16,16 +14,44 @@ public class EchoHandler extends Log implements ISORequestListener {
     @Override
     public boolean process(ISOSource source, ISOMsg m) {
         try {
+            logISOMsg(m);
             if (m.getMTI().equals(MTI_0800)) {
-                m.setResponseMTI();
-                m.set(39, "00");
-                source.send(m);
+                ISOMsg isoResp = (ISOMsg) m.clone();
+                isoResp.setResponseMTI();
+                isoResp.set(7, getCurISODateTime());
+                isoResp.set(39, "00");
+                logISOMsg(isoResp);
+                source.send(isoResp);
             }
 
         } catch (ISOException  | IOException e) {
             warn("echo-handler", e);
         }
         return true;
+    }
+
+    private void logISOMsg(ISOMsg msg) throws ISOException {
+        info("-----------------------------------");
+        if (msg.isRequest()) {
+            info("REQUEST MESSAGE");
+        } else if (msg.isResponse()) {
+            info("RESPONSE MESSAGE");
+        }
+        info(msg.getMTI());
+        int maxField = msg.getMaxField();
+        for(int i=1; i<=maxField; i++) {
+            if (msg.hasField(i)) {
+                info("Field - " + i + ": " + msg.getString(i));
+            }
+        }
+        info("-----------------------------------");
+
+    }
+
+    private String getCurISODateTime() {
+        Date date = new Date();
+        return ISODate.getDateTime(date);
+
     }
 
 
